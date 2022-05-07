912b61ceced16af206b7517375ccff3be0afb7ee
lx <2904937506@qq.com>
2022-05-07 22:55:12 +0800

    Springboot-静态资源

---
+++ 08-SpringBoot-06-StaticResource/src/main/java/com/fc/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.fc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String goHome() {
+        System.out.println("执行此方法");
+        return "hello.html";
+    }
+}
