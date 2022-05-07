58f0caf1d13633d09dbf7f7b52c84b1eada97c6c
lx <2904937506@qq.com>
2022-05-07 23:00:20 +0800

    Springboot-文件上传

---
+++ 08-SpringBoot-14-FileUpload/src/main/java/com/fc/util/FileUploadUtil.java
@@ -0,0 +1,41 @@
+package com.fc.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileUploadUtil {
+    public static String fileUpload(MultipartFile file) {
+        // 准备路径
+        String path = "D:\\apache-tomcat-8.5.37(1)\\webapps\\upload";
+
+        File locationFile = new File(path);
+
+        // 路径不存在就创建一个
+        if (!locationFile.exists()) {
+            locationFile.mkdirs();
+        }
+
+        String filename = file.getOriginalFilename();
+
+        String suffix = filename.substring(filename.lastIndexOf("."));
+
+        DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+
+        String prefix = formatter.format(new Date());
+
+        filename = prefix + suffix;
+
+        try {
+            file.transferTo(new File(locationFile, filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "http://localhost:8081/upload/" + filename;
+    }
+}
